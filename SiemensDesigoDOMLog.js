2fc6ba30655e8e4f1b1024c1f6de996aeecf1de3
xzovyAtWork <jsteinca@jci.com>
2024-07-22 12:17:11 -0600

    update sump status

--- SiemensDesigoDOMLog.js
+++ SiemensDesigoDOMLog.js
@@ -103,7 +103,7 @@ let sf5 = new Device(37, 'sf5');
 let sf6 = new Device(38, 'sf6');
 // let vfdHOA = new Device(40, 'VFD HOA');
 let vfdFault = new Device(39, 'VFD Fault');
-let sumpStatus = new Device( 11,'Pump Status');
+let sumpStatus = new Device( 12,'Pump Status');
 
 
 // let sensorList = [maTemp, saTemp, rh1, rh2, conductivity]
