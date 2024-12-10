17700cc7d54e33abe749b1420ba6751c3c7c2251
xzovyAtWork <jsteinca@jci.com>
2024-12-10 10:32:21 -0700

    Update alcLog.js
    Add network request function postReq

--- alcLog.js
+++ alcLog.js
@@ -2,6 +2,48 @@
 //add auto download and clear interval if they are set 4/6
 //this has updated checkPrevious() method 3/14
 
+let realmCount = main.getActionWindow().primitiveRequestRealm.realmCount;
+let wbs = main.getActionWindow().primitiveRequestRealm.wbsId;
+
+const fans = "prim_2178";
+const sump = "prim_2149";
+const bleed = "prim_2120";
+const drain = "prim_2091";
+const fill = "prim_2061";
+
+const sfSpeed = "prim_1709";
+const bypass = "prim_2019";
+const face = "prim_1975";
+
+const headerObj = {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/xml;charset=UTF-8",
+    "sec-ch-ua": "\"Google Chrome\";v=\"131\", \"Chromium\";v=\"131\", \"Not_A Brand\";v=\"24\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Windows\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin"
+}
+const refUrl = `http://localhost:8080/~dbid/1651?a=properties&c=default&i=equipment&f=io_points&wbs=${wbs}&pageCount=1&lcount=0`;
+
+//template function ie postReq(fans, 0); 0 is off, 1 is on
+function postReq(device, command = 0){
+    
+    const body = `<MESSAGES channelId=\"publisher\" realmId=\"primitiveRequestRealm\"><MESSAGE messageTypeId=\"reqPrimitiveSubMessage\" consumerId=\"PrimitiveRegistrant\" messageId=\"primitiveMessageSubmit\" priority=\"1\" realmCount="${realmCount}" seqnum=\"13\"><BODY><PRIMITIVE_SUBMIT getFieldValues=\"true\" updateDeferredValues=\"true\" updateActionSet=\"true\" auditlog=\"Edit checkout for i/o points\" auditenabled=\"true\" auditdetails=\"\" cjDoCommit=\"true\" cjGetChangesFromCore=\"true\"><PRIMITIVE id="${device}"><![CDATA[${command}]]></PRIMITIVE></PRIMITIVE_SUBMIT></BODY></MESSAGE></MESSAGES>`;
+
+    return fetch(`http://localhost:8080/_common/servlet/lvl5/msgservlet?wbs=${wbs}`, {
+        "headers": headerObj,
+        "referrer": refUrl,
+        "referrerPolicy": "strict-origin-when-cross-origin",
+        "body": body,
+        "method": "POST",
+        "mode": "cors",
+        "credentials": "include"
+    });
+};
+
 const aContent = document.querySelector('.actionFrame').contentWindow.document.childNodes[1];
 let lastPushed;
 class Device{
